22cde4cc5d64b29b29a965ed88889dee796b3c4a
Ishita Khanna <ishitakhanna2208@gmail.com>
2025-06-06 00:44:11 +1000

    Merge base and flight data using parsed origin field

--- rfds_airbase_analysis/app.R
+++ rfds_airbase_analysis/app.R
@@ -9,3 +9,11 @@ bases <- read_csv("data/RFDS_bases.csv")
 # Looking at dataset
 glimpse(flight_summary)
 glimpse(bases)
+
+# Splitting origin and joining it with base dataset
+
+df <- strsplit(flight_summary$Origin, " ")
+a <- lapply(df, function(x) x[1])
+flight_split <- mutate(flight_summary, location = a)
+location_summary <- merge(x = bases, y = flight_split, by.x = c("Location"), by.y = c("location"))
+
