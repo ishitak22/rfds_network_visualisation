4aa32cc67a898e2d7b78674c1725a8aec5a9517e
Ishita Khanna <ishitakhanna2208@gmail.com>
2025-06-09 00:55:42 +1000

    Made ui layout with title, map base and explaination for user

--- app.R
+++ app.R
@@ -4,3 +4,11 @@ library(leaflet)
 
 location_summary <- read.csv("data/finaldataset.csv")
 
+ui <- fluidPage(
+  titlePanel("RFDS Bases across Australia - July 2022"),
+  
+  leafletOutput("map", width = "100%", height = "700px"),
+  
+  p("This map shows Royal Flying Doctor Service (RFDS) bases where flights occurred in July 2022"),
+  p("Click any marker to see the destination airport name")
+)
